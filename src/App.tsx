85a6be6ae4b398df8c46cefb95f9d41a521780ec
Santiago Alzamora <santiago.alzamora@sandinas.com.ar>
2024-08-19 10:24:35 -0300

    rosie commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,38 @@
-import { useState } from 'react'
-import { IHoroscopo, calcularSigno, getHoroscopo, signTranslations } from './utils';
+// import { useState } from 'react'
+// import { IHoroscopo, calcularSigno, getHoroscopo, signTranslations } from './utils';
 import './App.css'
-import Loading from 'react-loading';
+// import Loading from 'react-loading';
 
 
 function App() {
-  const [horoscopo, setHoroscopo] = useState<IHoroscopo>()
-  const [selectedButton, setSelectedButton] = useState("daily")
-  const [signo, setSigno] = useState<string>();
-  const [loading, setLoading] = useState(false)
+  // const [horoscopo, setHoroscopo] = useState<IHoroscopo>()
+  // const [selectedButton, setSelectedButton] = useState("daily")
+  // const [signo, setSigno] = useState<string>();
+  // const [loading, setLoading] = useState(false)
 
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    const form = e.target as HTMLFormElement
-    const data = new FormData(form)
-    const date = data.get("date")?.toString()
+  // async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  //   e.preventDefault()
+  //   const form = e.target as HTMLFormElement
+  //   const data = new FormData(form)
+  //   const date = data.get("date")?.toString()
 
-    if (!date) return
-    if (new Date(date) > new Date()) {
-      alert("Ingrese una fecha valida")
-      return
-    }
-    setLoading(true)
-    const sign = calcularSigno(date)
-    const newSigno: string = signTranslations[sign]
+  //   if (!date) return
+  //   if (new Date(date) > new Date()) {
+  //     alert("Ingrese una fecha valida")
+  //     return
+  //   }
+  //   setLoading(true)
+  //   const sign = calcularSigno(date)
+  //   const newSigno: string = signTranslations[sign]
 
-    setSigno(newSigno)
-    const newHoroscopo = await getHoroscopo(sign)
+  //   setSigno(newSigno)
+  //   const newHoroscopo = await getHoroscopo(sign)
     
-    setHoroscopo(newHoroscopo)
+  //   setHoroscopo(newHoroscopo)
     
-    setLoading(false)
+  //   setLoading(false)
     
-  }
+  // }
 
 
   return (
