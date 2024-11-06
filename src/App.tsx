f40f2173b7393437563c4fdf121d4064abab3bfc
Santiago Alzamora <santiago.alzamora@sandinas.com.ar>
2024-11-06 09:42:38 -0300

    romi

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,45 @@
-import { useState } from 'react'
-import { IHoroscopo, calcularSigno, getHoroscopo, signTranslations } from './utils';
+// import { useState } from 'react'
+// import { IHoroscopo, calcularSigno, getHoroscopo, signTranslations } from './utils';
 import './App.css'
-import Loading from 'react-loading';
+// import Loading from 'react-loading';
 
 
 function App() {
-  const [horoscopo, setHoroscopo] = useState<IHoroscopo>()
-  const [selectedButton, setSelectedButton] = useState("daily")
-  const [signo, setSigno] = useState<string>();
-  const [loading, setLoading] = useState(false)
+  // const [horoscopo, setHoroscopo] = useState<IHoroscopo>()
+  // const [selectedButton, setSelectedButton] = useState("daily")
+  // const [signo, setSigno] = useState<string>();
+  // const [loading, setLoading] = useState(false)
 
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    const form = e.target as HTMLFormElement
-    const data = new FormData(form)
-    const date = data.get("date")?.toString()
+  // async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  //   e.preventDefault()
+  //   const form = e.target as HTMLFormElement
+  //   const data = new FormData(form)
+  //   const date = data.get("date")?.toString()
 
-    if (!date) return
-    if (new Date(date) > new Date()) {
-      alert("Ingrese una fecha valida")
-      return
-    }
-    setLoading(true)
-    const sign = calcularSigno(date)
-    const newSigno: string = signTranslations[sign]
+  //   if (!date) return
+  //   if (new Date(date) > new Date()) {
+  //     alert("Ingrese una fecha valida")
+  //     return
+  //   }
+  //   setLoading(true)
+  //   const sign = calcularSigno(date)
+  //   const newSigno: string = signTranslations[sign]
 
-    setSigno(newSigno)
-    const newHoroscopo = await getHoroscopo(sign)
+  //   setSigno(newSigno)
+  //   const newHoroscopo = await getHoroscopo(sign)
     
-    setHoroscopo(newHoroscopo)
+  //   setHoroscopo(newHoroscopo)
     
-    setLoading(false)
+  //   setLoading(false)
     
-  }
+  // }
 
 
   return (
     <main>
-      <h1>Calcula tu <span>Signo</span>, lee tu <span>Horoscopo</span></h1>
-      <form onSubmit={handleSubmit}>
+      <h1>Hola, <span>Romi</span>, lee tu <span>Horoscopo</span></h1>
+      <p>Debes dejar de mimirte a las 5am. Hoy vas a tener un buen dia y todo va a salir joya. No queda nada, tenes que seguir metiendole. Saludos, el <span>Santi</span></p>
+      {/* <form onSubmit={handleSubmit}>
         <input type="date" name="date" required={true} />
         <button>Submit</button>
       </form>
@@ -49,7 +50,7 @@ function App() {
       </section>
       {loading && <div style={{marginTop:16}}><Loading type='spinningBubbles'/></div>}
       {selectedButton === "daily" && <p>{horoscopo?.daily}</p>}
-      {selectedButton === "monthly" && <p>{horoscopo?.monthly}</p>}
+      {selectedButton === "monthly" && <p>{horoscopo?.monthly}</p>} */}
       </main>
   )
 }
